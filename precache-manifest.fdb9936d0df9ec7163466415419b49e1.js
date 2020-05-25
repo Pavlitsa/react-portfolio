650240f6a3d14b46527cf8df768eb8874108a593
Pavlitsa <pzafeiri@gmail.com>
2020-05-26 01:25:14 +0200

    Updates

---
+++ precache-manifest.fdb9936d0df9ec7163466415419b49e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab69fd02829c72e8400c7bac0da7338",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "1a951ae01e0fb6d36872",
+    "url": "/react-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c2330335fdb050d0c9c1",
+    "url": "/react-portfolio/static/css/main.bedf5db7.chunk.css"
+  },
+  {
+    "revision": "1a951ae01e0fb6d36872",
+    "url": "/react-portfolio/static/js/2.d6f26213.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.d6f26213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2330335fdb050d0c9c1",
+    "url": "/react-portfolio/static/js/main.5a1fd068.chunk.js"
+  },
+  {
+    "revision": "bee14ebf850387594d39",
+    "url": "/react-portfolio/static/js/runtime-main.8c64cdeb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.21c52600bfba2911c5e50a84b53e6518.js"
+  "/react-portfolio/precache-manifest.fdb9936d0df9ec7163466415419b49e1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
