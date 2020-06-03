24d4a968650088794e9b54745c7d74c8b0811f23
Pavlitsa <pzafeiri@gmail.com>
2020-06-03 23:38:38 +0200

    trying to fix deployment

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
         <Navbar />
         <Route exact path="/" component={Home} />
         <Route exact path="/home" component={Home} />
-        <Route exact path="/react-portfolio" component={Home} />
+        {/* <Route exact path="/react-portfolio" component={Home} /> */}
         <Route exact path="/about" component={About} />
         <Route exact path="/projects" component={Projects} />
       </div>
